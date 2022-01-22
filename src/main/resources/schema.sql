930a3b69a3cd4ba4ec350d80784a3f040a098920
Fernando Espinosa <Fernando_Espinosa@epam.com>
2022-01-22 17:19:28 -0400

    Add script db creation

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,26 @@
+create table PHONE
+(
+    phone_id     binary not null,
+    city_code    varchar(255),
+    country_code varchar(255),
+    number       varchar(255),
+    user_id      binary,
+    primary key (phone_id)
+);
+create table USER
+(
+    user_id    binary  not null,
+    active     boolean not null,
+    created    timestamp,
+    email      varchar(255),
+    last_login timestamp,
+    modified   timestamp,
+    name       varchar(255),
+    password   varchar(255),
+    token      varchar(255),
+    primary key (user_id)
+);
+alter table USER
+    add constraint UK_email unique (email);
+alter table PHONE
+    add constraint FK_user_phone foreign key (user_id) references user;
