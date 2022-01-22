6bb90326a5b310ef78f4210f3827ca97413d46ae
Fernando Espinosa <Fernando_Espinosa@epam.com>
2022-01-22 17:44:55 -0400

    Fix failing test

--- src/test/java/com/nisum/evaluation/service/JwtTokenGeneratorTest.java
+++ src/test/java/com/nisum/evaluation/service/JwtTokenGeneratorTest.java
@@ -1,14 +1,16 @@
 package com.nisum.evaluation.service;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
-@ActiveProfiles("test")
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(initializers = ConfigDataApplicationContextInitializer.class, classes = JwtTokenGenerator.class)
 class JwtTokenGeneratorTest {
 
     @Autowired
